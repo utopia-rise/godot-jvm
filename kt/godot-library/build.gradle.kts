7d04578c33a93e5c1677e0e1aef24eb42d49e7bf
Cedric Hippmann <cedric.hippmann@hotmail.com>
2021-02-04 11:17:01 +0100

    Build and copy `godot-bootstrap.jar` on scons build

--- kt/godot-library/build.gradle.kts
+++ kt/godot-library/build.gradle.kts
@@ -1,7 +1,16 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     kotlin("jvm")
+    id("com.github.johnrengelman.shadow") version "6.1.0"
 }
 
 dependencies {
     api(project(":godot-runtime"))
+}
+
+tasks.withType<ShadowJar> {
+    archiveBaseName.set("godot-bootstrap")
+    archiveVersion.set("")
+    archiveClassifier.set("")
 }
